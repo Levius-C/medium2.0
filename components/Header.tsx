a0b5bddd3e80d91ecf6171605def245a6878924a
Lev <a183492765@gmail.com>
2022-11-26 23:36:45 +0800

    header content fixed, hero section made

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,20 +2,22 @@ import Link from "next/link"
 
 function Header() {
   return (
-    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+    <header className="flex justify-between p-5 max-w-7xl mx-auto text-sm">
         <div className="flex items-center space-x-5">
             <Link href="/">
                     <img className="object-contain cursor-pointer w-44" src="https://links.papareact.com/yvf" alt="LOGO" />
             </Link>
-            <div className="hidden md:inline-flex items-center space-x-5">
-                <h3>About</h3>
-                <h3>Contact</h3>
-                <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
-            </div>
         </div>
-        <div className="flex items-center space-x-5 text-green-600">
-            <h3>Sign In</h3>
-            <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+        <div className="flex items-center space-x-5 text-slate-900">
+          <div className="hidden md:inline-flex items-center space-x-5">
+            <Link href="/ourstory">Our story</Link>
+            <Link href="/membership">Membership</Link>
+            <Link href="/write">Write</Link>
+          </div>
+          <div className="hidden sm:inline-flex items-center space-x-5">
+            <button>Sign In</button>
+          </div>
+            <button className="text-white bg-slate-900 px-5 py-2 rounded-full">Get Started</button>
         </div>
 
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,25 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Medium 2.0</title>
         <link rel="icon" href="https://links.papareact.com/yvf" />
       </Head>
+      
       <Header />
+
+      <div className="flex justify-between items-center  border-y bg-amber-400 border-black py-10 lg:py-0">
+        <div className="px-10 space-y-8">
+          <h1 className="text-6xl md:text-7xl lg:text-8xl max-w-xl font-serif font-medium">Stay curious.</h1>
+          <h2 className="text-xl font-medium">Discover stories, thinking, and expertise from writers on any topic.</h2>
+          <button className="text-white bg-slate-900 px-10 py-2 rounded-full text-xl">
+            Start reading
+          </button>
+        </div>
+        <img className="hidden md:inline-flex h-64 lg:h-full" src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="Medium LOGO" />
+      </div>
+
     </div>
   )
 }
